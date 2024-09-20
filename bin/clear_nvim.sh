d8ff9c5ccd595789433243fa642f023539313cc6
recelsus <rhinfunc+git@gmail.com>
2024-09-21 06:13:43 +0900

    create clear shellscript

---
+++ bin/clear_nvim.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+SHARE_DIR="$HOME/.local/share/nvim"
+CACHE_DIR="$HOME/.cache/nvim"
+
+echo "Delete Neovim Cache"
+
+# ~/.local/share/nvim
+if [ -d "$SHARE_DIR" ]; then
+  echo "DELETE $SHARE_DIR" 
+  rm -rf "$SHARE_DIR"
+else
+  echo "NOT FOUND $SHARE_DIR"
+fi
+#
+# ~/.cache/nvim の削除
+if [ -d "$CACHE_DIR" ];then
+  echo "DELETE $CACHE_DIR"
+  rm -rf "$CACHE_DIR"
+else
+  echo "NOT FOUND $CACHE_DIR"
+fi
+
+echo "COMPLATE"
