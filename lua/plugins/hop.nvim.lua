644ddc1da69568be4beb2af268ddb4f1c730f6b0
recelsus <rhinfunc+git@gmail.com>
2024-09-23 13:29:58 +0900

    add hop.nvim

---
+++ lua/plugins/hop.nvim.lua
@@ -0,0 +1,12 @@
+return {
+  "phaazon/hop.nvim",
+  branch = "v2",
+  config = function()
+    require("hop").setup {
+      multi_windows = true,
+    }
+  end,
+  keys = {
+    {mode = "", "<leader>s", "<cmd>HopWord<CR>", desc = ""},
+  }
+}
