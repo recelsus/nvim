7e5397860af8067f0a7acd2426fa8b7e55626dd8
recelsus <rhinfunc+git@gmail.com>
2024-09-29 00:20:13 +0900

    feat(lsp): Use simple `<C-p>`/`<C-n>` mappings for LSP completion
    This change removes the custom mappings for `<C-j>` and `<C-k>`
    in favor of simpler `<C-p>` and `<C-n>` mappings for navigating
    LSP completion suggestions.
    
    This aligns with the more intuitive and standard behavior
    of using these keys for navigation in completion menus.

--- lua/plugins/nvim-lsp.lua
+++ lua/plugins/nvim-lsp.lua
@@ -60,6 +60,8 @@ return {
           end,
         },
         mapping = {
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-s>'] = cmp.mapping.complete(),
@@ -68,24 +70,6 @@ return {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
           }),
-         -- ['<C-j>'] = cmp.mapping(function(fallback)
-         --   if cmp.visible() then
-         --     cmp.select_next_item()
-         --   elseif luasnip.expand_or_jumpable() then
-         --     luasnip.expand_or_jump()
-         --   else
-         --     fallback()
-         --   end
-         -- end, { 'i', 's' }),
-         -- ['<C-k>'] = cmp.mapping(function(fallback)
-         --   if cmp.visible() then
-         --     cmp.select_prev_item()
-         --   elseif luasnip.jumpable(-1) then
-         --     luasnip.jump(-1)
-         --   else
-         --     fallback()
-         --   end
-         -- end, { 'i', 's' }),
         },
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
