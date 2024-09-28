40b1771f4cfbf644d67a0c61e7f6316de75d7581
recelsus <rhinfunc+git@gmail.com>
2024-09-28 14:02:43 +0900

    feat(vim-translator): Add visual mode keybinding
    This commit adds a visual mode keybinding `<leader>tt` to translate the selected text using vim-translator.

--- lua/plugins/vim-translator.lua
+++ lua/plugins/vim-translator.lua
@@ -9,6 +9,7 @@ return {
       
       vim.keymap.set('n', '<leader>tt', '<Plug>Translate', { noremap = true, silent = true, desc = "[Translator] Translate Word" })
       vim.keymap.set('v', '<leader>tt', '<Plug>TranslateV', { noremap = true, silent = true, desc = "[Translator] Translate Selected Text" })
+      vim.keymap.set('x', '<leader>tt', '<Plug>TranslateV', { noremap = true, silent = true, desc = "[Translator] Translate Selected Text" })
       vim.keymap.set('n', '<leader>tw', '<Plug>TranslateW', { noremap = true, silent = true, desc = "[Translator] Translate Word in Window" })
       vim.keymap.set('n', '<leader>tr', '<Plug>TranslateR', { noremap = true, silent = true, desc = "[Translator] Replace Word with Translation" })
       vim.keymap.set('v', '<leader>tr', '<Plug>TranslateRV', { noremap = true, silent = true, desc = "[Translator] Replace Selected Text with Translation" })
