c4eb1e2fddf28162426e765ee09548a3c739a8da
recelsus <rhinfunc+git@gmail.com>
2024-09-29 05:16:09 +0900

    feat: Add command to yank last terminal command output
    This commit introduces a new command, accessible via `<C-y>` in normal and terminal mode, that allows users to yank the output of the last executed command in the terminal.
    
    The command works by searching for lines matching a prompt pattern and extracting the lines between the last two occurrences of the pattern. These lines are then yanked into the default register.
    
    This functionality improves the workflow for users who frequently interact with the terminal and need to copy command output.

--- lua/config/command.lua
+++ lua/config/command.lua
@@ -28,3 +28,49 @@ end
 
 vim.keymap.set('n', '<leader>td', translate_diagnostic_message, { noremap = true, silent = true, desc = "[LSP] Translate Diagnostic Message" })
 
+function YankLastCommandOutput()
+  
+  if vim.bo.buftype ~= "terminal" then
+    print("This is not a terminal buffer!")
+    return
+  end
+  
+  local prompt_pattern = "%[.*@.*% .*(.*) NL%]%$.*$"
+  
+  local last_line_num = vim.api.nvim_buf_line_count(0)
+  local lines = vim.api.nvim_buf_get_lines(0, 0, last_line_num, false)
+
+  local start_line = nil
+  local end_line = nil
+  
+  for i = last_line_num, 1, -1 do
+    if lines[i]:match(prompt_pattern) then
+      if not end_line then
+        
+        end_line = i
+      elseif not start_line then
+        
+        start_line = i + 1
+        break
+      end
+    end
+  end
+  
+  if start_line and end_line then
+    
+    local yank_start = start_line
+    local yank_end = end_line - 1
+    
+    if yank_start <= yank_end then
+      vim.cmd(string.format("%d,%dyank", yank_start, yank_end))
+      print("Yanked lines from last command output!")
+    else
+      print("No command output to yank!")
+    end
+  else
+    print("Could not find two prompt patterns!")
+  end
+end
+
+vim.api.nvim_set_keymap('n', '<C-y>', ':lua YankLastCommandOutput()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', '<C-y>', [[<C-\><C-n>:lua YankLastCommandOutput()<CR>]], { noremap = true, silent = true })
