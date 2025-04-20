fd58614e72907b39cf5c104dfc521a9d25c1893e
recelsus <rhinfunc+git@gmail.com>
2025-04-21 00:29:12 +0900

    Refactor: Update hop.nvim plugin and improve keymap
    This commit updates the hop.nvim plugin to use smoka7/hop.nvim with versioning and improves the keymap for jumping to words.
    
    - lua/plugins/hop.lua
      - Changed plugin repository from phaazon/hop.nvim to smoka7/hop.nvim.
      - Updated branch to version = "*".
      - Added a description to the keymap for HopWord.

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -1,14 +1,14 @@
 return {
   {
-    "phaazon/hop.nvim",
-    branch = "v2",
+    "smoka7/hop.nvim",
+    version = "*",
     event = 'BufReadPre',
 
     config = function()
       require("hop").setup {
         multi_windows = true,
       }
-      
+
       vim.keymap.set("n", "<C-f>", "<cmd>HopWord<CR>", { noremap = true, silent = true, desc = "[Hop] Jump to Word" })
     end
   }
