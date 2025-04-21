9953d5cdb90a3ff3cc03e21fcfbc9e130b8188ab
recelsus <rhinfunc+git@gmail.com>
2025-04-21 19:46:52 +0900

    feat: Configure lsp_signature plugin
    This commit configures the lsp_signature plugin with custom options for improved usability.
    
    - lua/plugins/lsp-signature.lua
        - Added configuration options for the plugin, including enabling binding, floating window, border style, and disabling hints.
        - Added a setup function to initialize the plugin with the specified options.

--- lua/plugins/lsp-signature.lua
+++ lua/plugins/lsp-signature.lua
@@ -3,6 +3,16 @@ return {
     "ray-x/lsp_signature.nvim",
     event = "InsertEnter",
     opts = {
+      bind = true,
+      floating_window = true,
+      handler_opts = {
+        border = "single",
+      },
+      hint_enable = false,
     },
-  }
+    config = function(_, opts)
+      require("lsp_signature").setup(opts)
+    end,
+  },
 }
+
